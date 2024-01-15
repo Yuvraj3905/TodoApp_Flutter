454fc1651c316fc55a8bbebb0a4830d1b2ed6c6d
yuvraj3905 <74104618+Yuvraj3905@users.noreply.github.com>
2024-01-15 17:44:37 +0530

    Update home.dart

--- todo/lib/screens/home.dart
+++ todo/lib/screens/home.dart
@@ -7,20 +7,54 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween, 
-          children: [
-            Icon(Icons.menu, color: tdBlack, size: 30),
-            Container(
-              height: 40,
-              width: 40,
-            child: ClipRRect(borderRadius:BorderRadius.circular(20),child:Image.asset('assets/images/avatar.jpg')),)
-          ],
-        ),
-      ),
-      body: Container(child: Text('This is home screen.')),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(children: [searchBox()])),
     );
   }
+}
+
+Widget searchBox() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: const TextField(
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey)),
+    ),
+  );
+}
+
+AppBar _buildAppBar() {
+  return AppBar(
+    elevation: 0,
+    backgroundColor: tdBGColor,
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(Icons.menu, color: tdBlack, size: 30),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpg')),
+        )
+      ],
+    ),
+  );
 }
