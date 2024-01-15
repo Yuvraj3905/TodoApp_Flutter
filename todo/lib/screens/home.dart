720571cf46b58feaad54abb8f2de24abb1d1a186
yuvraj3905 <74104618+Yuvraj3905@users.noreply.github.com>
2024-01-15 17:04:50 +0530

    Completed with Navbar

--- todo/lib/screens/home.dart
+++ todo/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,10 +7,20 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text('TOdO App')),
-      body: Container(
-        child:Text('This is home screen.')
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+          children: [
+            Icon(Icons.menu, color: tdBlack, size: 30),
+            Container(
+              height: 40,
+              width: 40,
+            child: ClipRRect(borderRadius:BorderRadius.circular(20),child:Image.asset('assets/images/avatar.jpg')),)
+          ],
+        ),
       ),
+      body: Container(child: Text('This is home screen.')),
     );
   }
 }
